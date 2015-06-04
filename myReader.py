6ebd38c7f515f63da9db97b49348598ae5014d2b
Kevin Tan <kevin.tan@yale.edu>
2015-06-04 16:17:43 -0400

    read through files and create array to hold trees

--- myReader.py
+++ myReader.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import h5py
 
+a.f.close()
+
 class RockstarReader(object):
 
     def __init__(self, filename):
@@ -12,12 +14,15 @@ class RockstarReader(object):
 
         if not os.path.isfile(filename):
             raise IOError("Input filename %s is not a file" % filename)
-        self.f = h5py.File('sample1.hdf5', 'w')  
+        self.f = h5py.File('sample.hdf5', 'w')  
         self.fname = filename
         self._uncompress_ascii()
+        self.get_header()
+        self.forest = self.set_tree()
 
     def get_header(self):
         header = {}
+        hd_header = self.f.create_group('header')
         with open(self.fname) as ascii_file:
             header['ascii_header'] = []
             for i, line in enumerate(ascii_file):
@@ -26,22 +31,37 @@ class RockstarReader(object):
                 if(i==0):
                     header['categories'] = line.split()
                 if(line[0]!='#'):
+                    hd_header.attrs['length'] = i
                     break
-
-        hd_header = self.f.create_group('header')
         hd_header['categories'] = np.asarray(header['categories'])
         hd_header['ascii_header'] = header['ascii_header']
-        print header['ascii_header']
 
+    def set_tree(self):
+        f = self.f
+        with open(self.fname) as ascii_file:
+            #skip header lines
+            for x in xrange(f['header'].attrs['length']):
+                next(ascii_file)
 
+            forest = []
+            tree = []
+            #iterate through lines in file
+            tree_root_ID = ""
+            for line in ascii_file:
+                if(line[0]=='#'): #new tree
+                    forest.append(tree)
+                    tree = []
+                    tree_root_ID = line[6:]
+                    curr_group = f.create_group("tree"+tree_root_ID)
+                    curr_group.attrs['tree_root_ID'] = tree_root_ID
+                tree.append(line)
+                print line
+            return forest
+            # f['forest'] = forest
+
+            
+                
     def _uncompress_ascii(self):
-        """ If the input fname has file extension `.gz`, 
-        then the method uses `gunzip` to decompress it, 
-        and returns the input fname truncated to exclude 
-        the `.gz` extension. If the input fname does not 
-        end in `.gz`, method does nothing besides return 
-        the input fname. 
-        """
         if self.fname[-3:]=='.gz':
             print("...uncompressing ASCII data")
             os.system("gunzip "+self.fname)
@@ -50,3 +70,4 @@ class RockstarReader(object):
             pass
         return
 
+a = RockstarReader('sample.txt')
