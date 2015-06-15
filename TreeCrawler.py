7e636f5f66252f8cb9b05b9b4c50d0721994dace
Kevin Tan <kevin.tan@yale.edu>
2015-06-15 17:14:14 -0400

    generalized property derivative interpolated using scipy

--- TreeCrawler.py
+++ TreeCrawler.py
@@ -1,8 +1,10 @@
 import os
 import numpy as np
 import h5py
+import matplotlib.pyplot as plt
+from scipy.interpolate import InterpolatedUnivariateSpline
 DEPTH_ID_COL = 28
-Z_COL = 19
+Z_COL = 0
 
 
 def getTrunk(haloID, hdf5_file):
@@ -59,7 +61,6 @@ def generalized_formation_time(column, input_value, fraction, haloID, hdf5_file)
     print 'formation time', masked_table[i][column]
     return
 
-#Z - 19
 def generalized_property_derivative(column, z, dz, haloID, hdf5_file):
     """inputs: colum, input value, fraction, haloId, hdf5_file"""
    
@@ -69,43 +70,17 @@ def generalized_property_derivative(column, z, dz, haloID, hdf5_file):
 
     masked_table = table[trunk_mask] #filter table with mask
 
-    # find z
-    i = 0
-    found_z = False
-    while(i < len(trunk_mask) - 1):
-        if (abs(masked_table[i][Z_COL] - z) <= .01 ):
-            found_z = True
-            break
-        i+=1
-    z_index = i
-    z_value = masked_table[i][Z_COL]
+    print masked_table[:, Z_COL]
+    print masked_table[:, column]
+
+    spline = InterpolatedUnivariateSpline(list(reversed(masked_table[:, Z_COL])), list(reversed(masked_table[:,column])))
+    col_z = spline(z)
+    col_dz = spline((z+dz))
+
+    generalized_derivative = (col_dz - col_z)/(dz)
+    return generalized_derivative
 
 
-    #find index at dz
-    found_dz = False
-    if (found_z):
-        while(i > 0):
-            i-=1
-            delta_z = abs(masked_table[i][Z_COL] - z_value) 
-            if (abs(delta_z - dz) <= .01 ):
-                found_dz = True
-                break
-        dz_index = i
-        dz_value = masked_table[i][Z_COL]
-
-    if(found_z and found_dz):
-        print 'z index', z_index
-        print 'dz index', dz_index
-        # print 'scale factor', masked_table[i][0]
-        # print 'id', masked_table[i][1]
-        print 'z value', masked_table[z_index][Z_COL]
-        print 'dz value', masked_table[dz_indexge][Z_COL]
-    else:
-        if(not found_z):
-            print 'did not find z'
-        else:
-            print 'did not find dz'
-    
 
 
 
