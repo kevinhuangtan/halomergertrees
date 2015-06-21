02700328da0c1bac93287bb0875de1354a680d37
Kevin Tan <kevin.tan@yale.edu>
2015-06-21 16:24:21 -0400

    sorted by depth first with last main leaf

--- TreeCrawler.py
+++ TreeCrawler.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 from scipy.interpolate import InterpolatedUnivariateSpline
+LAST_MAINLEAF_DEPTHFIRST_ID = 34
 DEPTH_ID_COL = 28
 Z_COL = 0
 
@@ -12,43 +13,48 @@ def getTrunk(haloID, hdf5_file):
     mask = []
     with h5py.File(hdf5_file, "r") as f:
         tree = f[str(haloID)]
+        last_mainleaf = tree[0][LAST_MAINLEAF_DEPTHFIRST_ID]
         depth_id = tree[0][DEPTH_ID_COL] #first
-        for i in range(0,len(tree)):
-            if(tree[i][DEPTH_ID_COL] == (depth_id + 1)):# next in depth search
-                mask.append(i)
-                depth_id+=1
+        for i in range(0, last_mainleaf):
+            # if(tree[i][DEPTH_ID_COL] == (depth_id + 1)):# next in depth search
+            mask.append(i)
+                # depth_id+=1
     return mask
 
-def getTable(haloID, hdf5_file):
+def getTrunkTable(haloID, hdf5_file):
     with h5py.File(hdf5_file, "r") as f:
-        table = f[str(haloID)][...]
-    return table
+        last_mainleaf = int(f[str(haloID)].attrs.get('last_mainleaf'))
+        root = int(f[str(haloID)][0][DEPTH_ID_COL])
+        length = last_mainleaf - root
+        trunk = f[str(haloID)][0: (length + 1)]
+    return trunk
+
+#3061006267
 
 def print_trunk_column(column, haloID, hdf5_file):
     """print values of a given column of a tree's trunk"""
 
-    trunk_mask = getTrunk(haloID, hdf5_file) #get trunk
-
-    table = getTable(haloID, hdf5_file) #get table
-
-    masked_table = table[trunk_mask] #filter table with mask
+    masked_table = getTrunkTable(haloID, hdf5_file)
 
     #print column
-    for i in range(0, len(trunk_mask)):
+    for i in range(0, len(masked_table)): #O(1) for length function in any python object
         print masked_table[i][column]
 
 #mass - (10)
 def generalized_formation_time(column, input_value, fraction, haloID, hdf5_file):
     """inputs: colum, input value, fraction, haloId, hdf5_file"""
-
-    trunk_mask = getTrunk(haloID, hdf5_file) #get trunk
+    # trunk_mask = getTrunk(haloID, hdf5_file) #get trunk
     
-    formation_time = input_value * fraction #formation time
+    # formation_time = input_value * fraction #formation time
 
-    table = getTable(haloID, hdf5_file) #get table
+    # table = getTable(haloID, hdf5_file) #get table
 
     #filter table with mask
-    masked_table = table[trunk_mask]
+    masked_table = getTrunkTable(haloID, hdf5_file)
+
+    spline = InterpolatedUnivariateSpline(list(reversed(masked_table[:, Z_COL])), list(reversed(masked_table[:,column])))
+
+    spline()
 
     # find when reaches formation time
     i = 0
@@ -62,16 +68,19 @@ def generalized_formation_time(column, input_value, fraction, haloID, hdf5_file)
     return
 
 def generalized_property_derivative(column, z, dz, haloID, hdf5_file):
-    """inputs: colum, input value, fraction, haloId, hdf5_file"""
+    """Solve for generalized property derivative at a given z
+
+    Parameters 
+    ----------
+    column, z, dz, haloID, hdf5 filename
+
+    Returns 
+    -------
+    Generalized Property Derivative (column(z1) - column(z0))/(z1-z0)
+    """
    
-    trunk_mask = getTrunk(haloID, hdf5_file) #get trunk
 
-    table = getTable(haloID, hdf5_file) #get table
-
-    masked_table = table[trunk_mask] #filter table with mask
-
-    print masked_table[:, Z_COL]
-    print masked_table[:, column]
+    masked_table = getTrunkTable(haloID, hdf5_file)
 
     spline = InterpolatedUnivariateSpline(list(reversed(masked_table[:, Z_COL])), list(reversed(masked_table[:,column])))
     col_z = spline(z)
--- asciiReader.py
+++ asciiReader.py
@@ -1,16 +1,22 @@
 import os
 import numpy as np
 import h5py
-
+DEPTH_ID_COL = 28
+LAST_MAINLEAF_DEPTHFIRST_ID = 34
 
 class RockstarReader(object):
 
     def __init__(self, filename, hdf5_name):
-        """
-        RockstarReader(filename)
-        """
-        
+        """A class that reads an ASCII merger tree and stores its header and tree data. Writes data to hdf5 file. 
 
+        Parameters 
+        ----------
+        ASCII file name, hdf5 file name
+
+        Returns 
+        -------
+        writes to hdf5 file
+        """
         if not os.path.isfile(filename):
             raise IOError("Input filename %s is not a file" % filename)
         # self.f = h5py.File('myReaderAppend.hdf5', 'w')  
@@ -21,6 +27,8 @@ class RockstarReader(object):
         self.read_in_trees()
 
     def get_header(self):
+        """ Finds header rows in ASCII File and stores them in hdf5 group "ascii_header"
+        """
         with h5py.File(self.hdf5_name,"w") as f: #write new hdf5
             header = {}
             h5_header = f.create_group('ascii_header')
@@ -39,6 +47,8 @@ class RockstarReader(object):
         return
 
     def read_in_trees(self):
+        """Store each tree in its own hdf5 dataset. Tree is sorted by Depth First ID for fast access to trunk
+        """
         with h5py.File(self.hdf5_name,"r+") as f:
             tree_id = ""
             with open(self.fname) as ascii_file:
@@ -53,7 +63,9 @@ class RockstarReader(object):
                 for line in ascii_file:
                     if(line[0]=='#'): #new tree
                         if(not first_line): #not first tree
-                            f[tree_id] = np.array(current_tree)
+                            arr = np.array(current_tree).astype('f8')
+                            f[tree_id] = arr[arr[:, DEPTH_ID_COL].argsort()] #sort by depthid
+                            f[tree_id].attrs['last_mainleaf'] = int(f[tree_id][0][LAST_MAINLEAF_DEPTHFIRST_ID]) #set last_mainleaf
                             current_tree = []
                             tree_index += 1
                         else:
@@ -62,8 +74,10 @@ class RockstarReader(object):
                         tree_id = line[6:].strip('\n')
                     else: #read in next tree element
                         current_tree.append(line.split())
-                f[tree_id] = np.array(current_tree).astype('f8')
-                
+                arr = np.array(current_tree).astype('f8')
+                f[tree_id] = arr[arr[:, DEPTH_ID_COL].argsort()]
+                f[tree_id].attrs['last_mainleaf'] = int(f[tree_id][0][LAST_MAINLEAF_DEPTHFIRST_ID])
+
         return 
             
     def _uncompress_ascii(self):
